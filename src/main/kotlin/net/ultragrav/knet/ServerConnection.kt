20da3e305468d54cdf1bd1542049fc528526b076
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-12-19 19:21:05 -0500

    Rename parent to attachment

--- src/main/kotlin/net/ultragrav/knet/ServerConnection.kt
+++ src/main/kotlin/net/ultragrav/knet/ServerConnection.kt
@@ -1,18 +1,13 @@
 package net.ultragrav.knet
 
 import io.netty.channel.Channel
-import net.ultragrav.knet.exception.DisconnectedException
 import net.ultragrav.knet.packet.packets.PacketProxyCall
-import net.ultragrav.knet.proxy.ProxyCallProviderImpl
 import net.ultragrav.knet.proxy.ProxyCaller
 
 class ServerConnection(val server: KNetServer, val channel: Channel) : ProxyCaller() {
-    lateinit var parent: Any
+    lateinit var attachment: Any
 
     override suspend fun sendCall(call: PacketProxyCall) {
-        if (!channel.isActive) {
-            throw DisconnectedException()
-        }
         channel.writeAndFlush(call).awaitKt()
     }
 
