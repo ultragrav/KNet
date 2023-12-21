e849151d2ad61976db51d8dd840cd04e443add3a
UltraDev <petzmagnus@gmail.com>
2023-12-21 20:53:38 +0100

    Add helper methods for registering proxies

--- src/main/kotlin/net/ultragrav/knet/proxy/ProxyRegistrar.kt
+++ src/main/kotlin/net/ultragrav/knet/proxy/ProxyRegistrar.kt
@@ -4,8 +4,14 @@ import net.ultragrav.knet.ProxyCallHandler
 
 interface ProxyRegistrar {
     fun <T> registerProxy(inter: Class<T>, proxy: ProxyCallHandler<T>)
+    fun <T> registerProxy(inter: Class<T>, proxy: T) {
+        registerProxy(inter, ProxyFactory.createHandler(inter, proxy))
+    }
 }
 
 inline fun <reified T> ProxyRegistrar.registerProxy(proxy: ProxyCallHandler<T>) {
+    registerProxy(T::class.java, proxy)
+}
+inline fun <reified T> ProxyRegistrar.registerProxy(proxy: T) {
     registerProxy(T::class.java, proxy)
 }
