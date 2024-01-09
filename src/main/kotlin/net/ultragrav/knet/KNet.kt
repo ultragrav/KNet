5d4c436dae1053de9de4e166842a6d8f185095c2
Matthew Lack <matt.lack@mail.utoronto.ca>
2024-01-09 02:51:54 -0500

    Change protobuf to KJsonSerializer.kt

--- src/main/kotlin/net/ultragrav/knet/KNet.kt
+++ src/main/kotlin/net/ultragrav/knet/KNet.kt
@@ -3,13 +3,13 @@ package net.ultragrav.knet
 import kotlinx.coroutines.Dispatchers
 import net.ultragrav.knet.proxy.KNetCallerContextElement
 import net.ultragrav.knet.proxy.ProxyCaller
-import net.ultragrav.knet.serialization.ProtoBufSerializer
+import net.ultragrav.knet.serialization.KJsonSerializer
 import net.ultragrav.knet.serialization.Serializer
 import kotlin.coroutines.coroutineContext
 
 object KNet {
     var defaultDispatcher = Dispatchers.Default
-    var serializer: Serializer = ProtoBufSerializer
+    var serializer: Serializer = KJsonSerializer
 
     suspend inline fun caller(): ProxyCaller {
         return coroutineContext[KNetCallerContextElement.Key]?.caller ?: error("No caller found in context")
--- src/main/kotlin/net/ultragrav/knet/serialization/KJsonSerializer.kt
+++ src/main/kotlin/net/ultragrav/knet/serialization/KJsonSerializer.kt
@@ -3,16 +3,17 @@ package net.ultragrav.knet.serialization
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.protobuf.ProtoBuf
 import kotlinx.serialization.serializer
+import net.ultragrav.kserializer.kotlinx.KJson
 import kotlin.reflect.KType
 
 @OptIn(ExperimentalSerializationApi::class)
-object ProtoBufSerializer : Serializer {
+object KJsonSerializer : Serializer {
     override fun <T> serialize(type: KType, obj: T): ByteArray {
-        return ProtoBuf.encodeToByteArray(serializer(type), obj)
+        return KJson.encodeToByteArray(serializer(type), obj)
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun <T> deserialize(type: KType, bytes: ByteArray): T {
-        return ProtoBuf.decodeFromByteArray(serializer(type), bytes) as T
+        return KJson.decodeFromByteArray(serializer(type), bytes) as T
     }
 }
