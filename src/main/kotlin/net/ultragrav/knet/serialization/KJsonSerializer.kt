40256a2f892f4f871b9098d454891ea96e32b9a2
Matthew Lack <matt.lack@mail.utoronto.ca>
2024-10-08 11:30:13 -0400

    add module to Serializer.kt

--- src/main/kotlin/net/ultragrav/knet/serialization/KJsonSerializer.kt
+++ src/main/kotlin/net/ultragrav/knet/serialization/KJsonSerializer.kt
@@ -3,11 +3,16 @@ package net.ultragrav.knet.serialization
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializer
 import net.ultragrav.kserializer.kotlinx.KJson
 import kotlin.reflect.KType
 
 object KJsonSerializer : Serializer {
+
+    override val module: SerializersModule
+        get() = KJson.defaultModule
+
     override fun <T> serialize(kxStrategy: SerializationStrategy<T>?, type: KType, obj: T): ByteArray {
         return KJson.encodeToByteArray(kxStrategy!!, obj)
     }
--- src/main/kotlin/net/ultragrav/knet/serialization/Serializer.kt
+++ src/main/kotlin/net/ultragrav/knet/serialization/Serializer.kt
@@ -2,19 +2,21 @@ package net.ultragrav.knet.serialization
 
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializer
 import net.ultragrav.kserializer.kotlinx.KJson
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
 interface Serializer {
+    val module: SerializersModule
     fun <T> serialize(kxStrategy: SerializationStrategy<T>?, type: KType, obj: T): ByteArray
     fun <T> deserialize(kxStrategy: DeserializationStrategy<T>?, type: KType, bytes: ByteArray): T
 }
 
 inline fun <reified T> Serializer.serialize(obj: T): ByteArray = serialize(
     try {
-        KJson.defaultModule.serializer()
+        module.serializer()
     } catch (e: Exception) {
         null
     },
@@ -22,7 +24,7 @@ inline fun <reified T> Serializer.serialize(obj: T): ByteArray = serialize(
 )
 inline fun <reified T> Serializer.deserialize(bytes: ByteArray): T = deserialize(
     try {
-        KJson.defaultModule.serializer()
+        module.serializer()
     } catch (e: Exception) {
         null
     },
